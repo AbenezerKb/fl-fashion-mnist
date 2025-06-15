39a84501a091fc885e731fce74d04e3594dc2099
AbenezerKb <aben.jacob3@gmail.com>
2025-06-15 16:12:28 +0000

    add context to client app

--- fl_fashion_mnist/client_app.py
+++ fl_fashion_mnist/client_app.py
@@ -5,7 +5,7 @@ import torch
 
 from random import random
 from flwr.client import ClientApp, NumPyClient
-from flwr.common import Context
+from flwr.common import Context, ConfigRecord
 from fl_fashion_mnist.task import Net, get_weights, load_data, set_weights, test, train
 
 import json
@@ -13,13 +13,17 @@ import json
 
 # Define Flower Client and client_fn
 class FlowerClient(NumPyClient):
-    def __init__(self, net, trainloader, valloader, local_epochs):
+    def __init__(self, net, trainloader, valloader, local_epochs, context: Context):
+        self.client_state = context.state
         self.net = net
         self.trainloader = trainloader
         self.valloader = valloader
         self.local_epochs = local_epochs
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.net.to(self.device)
+
+        if "fit_metrics" not in self.client_state.config_records:
+            self.client_state.config_records["fit_metrics"] = ConfigRecord()
 
     def fit(self, parameters, config):
         set_weights(self.net, parameters)
@@ -30,6 +34,13 @@ class FlowerClient(NumPyClient):
             self.local_epochs,
             self.device,
         )
+        
+        print(self.client_state)
+        fit_metrics = self.client_state.config_records["fit_metrics"]
+        if "train_loss_hist" not in fit_metrics:
+            fit_metrics["train_loss_hist"] = [train_loss]
+        else:
+            fit_metrics["train_loss_hist"].append(train_loss)
         complex_metrics = {"m1": random(), "m2": random(), "m3": random()}
         return (
             get_weights(self.net),
@@ -53,7 +64,7 @@ def client_fn(context: Context):
     local_epochs = context.run_config["local-epochs"]
 
     # Return Client instance
-    return FlowerClient(net, trainloader, valloader, local_epochs).to_client()
+    return FlowerClient(net, trainloader, valloader, local_epochs, context).to_client()
 
 
 # Flower ClientApp
